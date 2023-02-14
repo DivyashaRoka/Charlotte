c75b7d63aaef1ec65947d9b5965b3170215a1ed9
AbhinandVJ6 <abhinandvj6@outlook.com>
2023-02-14 09:17:42 +0530

    updated

---
+++ backend/zap/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+@login_required
+def home(request):
+    return render(request, 'index.html')
+
+
+def edit(request):
+    return render(request, 'edit.html')
