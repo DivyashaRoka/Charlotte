2087d141cafe2e23827e14189df6b7176ead3448
AbhinandVJ6 <abhinandvj6@outlook.com>
2023-02-14 09:18:43 +0530

    i did everything

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-)-0$nj4i!!_594fh&h=f+cy%=7#zxlbq0(50h+hqq#!hbj)r=2'
 
-# SECURITY WARNING: don't run with debug turned on in production.
+# SECURITY WARNING: don't run with debug turned on in production..
 DEBUG = True
 
 ALLOWED_HOSTS = []
